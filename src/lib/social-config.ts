e7b553c9d4068ad2352f004ee714e081c9ad5de1
armelyara <theday.ay@gmail.com>
2026-07-29 20:50:17 -0400

    chore: update Twitter handle in social-config.ts

--- src/lib/social-config.ts
+++ src/lib/social-config.ts
@@ -6,22 +6,22 @@
  */
 
 export const socialLinks = {
-    email: 'armelyara@thedayinfo.com',
-    twitter: 'https://x.com/ArmelYara',
-    facebook: 'https://www.facebook.com/thedayinfo/',
-    linkedin: 'https://www.linkedin.com/company/thedayinfo',
-    github: 'https://github.com/armelyara',
-  } as const;
-  
-  export const siteInfo = {
-    name: 'The Day Info',
-    author: 'Armel Yara',
-    description: "Blog d'actualités et de réflexions sur la technologie et l'innovation",
-    foundedYear: 2018,
-  } as const;
-  
-  export const legalLinks = {
-    mentionsLegales: '/mentions-legales',
-    politiqueConfidentialite: '/politique-confidentialite',
-    contact: '/contact',
-  } as const;
+  email: 'armelyara@thedayinfo.com',
+  twitter: 'https://x.com/the_day_info',
+  facebook: 'https://www.facebook.com/thedayinfo/',
+  linkedin: 'https://www.linkedin.com/company/thedayinfo',
+  github: 'https://github.com/armelyara',
+} as const;
+
+export const siteInfo = {
+  name: 'The Day Info',
+  author: 'Armel Yara',
+  description: "Blog d'actualités et de réflexions sur la technologie et l'innovation",
+  foundedYear: 2018,
+} as const;
+
+export const legalLinks = {
+  mentionsLegales: '/mentions-legales',
+  politiqueConfidentialite: '/politique-confidentialite',
+  contact: '/contact',
+} as const;
